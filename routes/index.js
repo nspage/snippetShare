1d2fb06b7bbbe34b402d8870dfc32e19a96ad915
ArielleWg <ariellewagner@orange.fr>
2019-04-28 15:27:12 +0200

    cssHeaderHome

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('account', { title: 'Express' });
+  res.render('index', { title: 'SnippetShare' });
 });
+
+/* GET account page. */
+router.get('/account', function(req, res, next) {
+  res.render('account', { title: 'Your snippetShare account' });
+});
+
 
 module.exports = router;
